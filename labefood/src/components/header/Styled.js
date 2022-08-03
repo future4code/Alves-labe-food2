caec07e18e331199248f5e2b90b506879ccf7132
Victor Leandro Eleuterio <victorleandro430@gmail.com>
2022-08-03 10:05:10 -0300

    Media pt1

--- labefood/src/components/header/Styled.js
+++ labefood/src/components/header/Styled.js
@@ -6,10 +6,16 @@ export const MainDiv = styled.div`
     background-color: white;
     display: flex;
     align-items: center;
+
     width: 360px;
     height: 44px;
     border-top: none;
     border-bottom: solid gray 1px;
+    @media(min-width: 415px) {
+        transform: translate(-50%, -50%);
+        left:50%;
+        top:20px;
+    }
 `;
 export const BackButtonDiv = styled.div`
     margin-left: 14px;
