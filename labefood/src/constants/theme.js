2a334e3873d4384ce79b30236b748ec705d706b7
Elison Moises da Silva Oliveira <ajatosdownload@gmail.com>
2022-08-03 00:47:26 -0300

    finalizando cadastro

--- labefood/src/constants/theme.js
+++ labefood/src/constants/theme.js
@@ -1,15 +1,17 @@
-import { createTheme } from '@mui/material';
-import { primaryColor, secundaryColor } from './color';
+import { createTheme } from "@material-ui/core";
+import { primaryColor, neutralColor } from "./color";
 
 const theme = createTheme({
-    palette: {
-        primary: {
-
-            main: primaryColor,
-            contrastText: 'white',
-
-        }
-    }
+  palette: {
+    primary: {
+      main: primaryColor,
+      contrastext: "white",
+    },
+    text: {
+      primary: neutralColor,
+      secondary: "#D0D0D0", //gray
+    },
+  },
 });
 
-export default theme
+export default theme;
